e22056c3b313c328d6babeb70aa2cba3d1b88469
Tobias Heinloth <Tobias.Heinloth@googlemail.com>
2026-01-29 22:34:34 +0100

    init commit

---
+++ CoffeeOracle/CoffeeOracle.py
@@ -0,0 +1,124 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+import base64
+import dotenv
+
+dotenv.load_dotenv()
+
+from openai import OpenAI
+
+
+client = OpenAI()
+
+
+
+class State(rx.State):
+    """The app state."""
+    result: str
+    show: bool = False
+    creativity: int = 5
+    name: str
+    uploaded_files: list[str] = []
+
+    @rx.event
+    async def handle_upload(
+        self, files: list[rx.UploadFile]
+    ):  
+        for file in files:
+            data = await file.read()
+            path = rx.get_upload_dir() / file.name
+            with path.open("wb") as f:
+                f.write(data)
+            self.uploaded_files.append(file.name)
+            break
+        
+        img = base64.b64encode(data).decode("utf-8")
+        self.show = True
+        self.result = ""
+        with client.responses.stream(
+           model="gpt-5-nano",
+            reasoning={"effort":"low"},
+            input=[
+                {
+                    "role": "system",
+                    "content": [
+                        { "type": "input_text", "text": f"""Du bist das weltbekannte Kaffeemilschaum Orakel. 
+                         Du hast erfahrung im Lesen von Milchschaum seit mehreren 100 Jahren. 
+                         Der Nutzer {self.name} möchte ein Leseung von dir erhalten. Er wünscht sich ein von 0 bis 10 esoterisches Ergebnis.
+                         Bitte erwähne nicht was der nutzer gewählt hat. Sondern nur das Eigentliche Orakel. 
+                         Er hat sich für den Wert {self.creativity} entschieden. Bitte lese die Tasse für Ihn und gibt ihm eine antwort.
+                         
+                         Der output sollte als nett formatierter markdown text sein""" },
+                    ],
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "input_image",
+                            "image_url": f"data:image/jpeg;base64,{img}",
+                        },
+                    ],
+                }
+            ],
+        ) as stream:
+
+            for event in stream:
+                # Inkrementeller Text
+                if event.type == "response.output_text.delta":
+                    print(event.delta, end="", flush=True)
+                    self.result += event.delta
+                    yield
+
+                # Optional: Tool Calls, Reasoning, etc.
+                elif event.type == "response.error":
+                    print("Fehler:", event.error)
+
+                elif event.type == "response.completed":
+                    print("\n\n--- RESPONSE COMPLETED ---")
+        
+        
+        
+
+    @rx.event
+    def sliderChange(self, value: list[float]):
+        print(value)
+        self.creativity = value
+
+    @rx.event
+    def updateName(self, value:str):
+        self.name = value
+
+
+
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to the famous Coffee Oracle!", size="9"),
+            rx.upload(id="upload"),
+        rx.button(
+            "Upload",
+            on_click=State.handle_upload(
+                rx.upload_files("upload")
+            ),
+        ),
+        rx.input(on_change=State.updateName,placeholder="Bitte Namen eingeben"),
+        rx.heading(f"Select Creativity: {State.creativity}"),
+        
+        rx.slider(min=0,max=10,default_value=State.creativity,on_value_commit=State.sliderChange,step=1,width="30%"),
+       
+        rx.cond(
+            State.show,
+            rx.markdown(State.result)
+        ),
+    )
+)
+
+
+app = rx.App()
+app.add_page(index)
